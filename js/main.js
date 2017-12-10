00fa0a7326e409bf2addd2be7a6323494d19e5d3
Vince Tse <vincetse@users.noreply.github.com>
2017-12-09 21:43:49 -0500

    Fix redir

--- js/main.js
+++ js/main.js
@@ -29,11 +29,6 @@ $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
 
-  // default to home path
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    window.location.replace('/')
-  }
-
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
